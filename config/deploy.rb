52b5e0c16a691096d4c811fc640f82c89e186a4e
Jon Baker <jon@miletbaker.com>
2011-09-26 17:08:41 +0100

    remove due to password exposure. Password changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,17 +39,6 @@ namespace :omop do
     run "ln -s #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   end
 
-  task :backup_database do
-    run "ls -xt #{shared_path}/db/" do |ch, stream, data|
-      files = data.split
-      if files.length > 5
-        files[5..files.length-1 ].each do |file|
-        run "rm #{shared_path}/db/#{file}"
-      end
-      end
-    end
-    run "mysqldump -u omop_production --password=Yih71CQLKA omop_production > #{shared_path}/db/#{releases.last}.sql"
-  end
 
 end
 after "deploy:setup", "omop:create_db_backup_dir"
