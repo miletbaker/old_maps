4259aa2537fc639c370c96cbae7697834529c6eb
Jon Baker <jon@miletbaker.com>
2011-09-26 19:47:46 +0100

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ OldMaps::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( admin.css ie.css maps.js )
+  config.assets.precompile += %w( admin/admin.css ie.css maps.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
